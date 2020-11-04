cdd0443d5cb9a203a1f8b2f9b6b7325cae64473f
shmagi <shmagigavashelashvili@yahoo.com>
2020-11-04 11:52:06 +0400

    Code refactorin: switching class-based components to functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import axios from 'axios';
 import Questions from './Components/Questions';
@@ -6,114 +6,87 @@ import Form from './Components/form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowAltCircleUp } from '@fortawesome/free-solid-svg-icons';
 
-class App extends Component {
-   constructor(props){
-     super(props);
-     this.state = {
-        data: [],
-        guessedAnswers : 0,
-        isCorrect: false,
-        inCorrect : false,
-        endTest : false,
-        ArrowUpIcon : false
-     };
-   this.handleAnswer = this.handleAnswer.bind(this);
-   this.reset = this.reset.bind(this);
-   this.endtest = this.endtest.bind(this);
-   };
+function App () {
+  const [data, setData ] = useState([]);
+  const [guessedAnswers, setGuessedAnswers ] = useState(0);
+  const [isCorrect, setIsCorrect ] = useState(false);
+  const [inCorrect, setInCorrect ] = useState(false);
+  const [endTest, setEndTest ] = useState(false);
+  const [ArrowUpIcon, setArrowUpIcon ] = useState(false);
 
    // Fetchs Question data from API
-   getQuestions = async (category, dificulty, type) => {
+   const getQuestions = async (category, dificulty, type) => {
        // fetchts question data when category dificulty and type is chosen, All three of them
       if(category && dificulty && type){
         const APi_call = await axios.get(`https://opentdb.com/api.php?amount=7&category=${category}&difficulty=${dificulty}&type=${type}`);
         const response = APi_call.data.results;
-        this.setState({
-          data : response,
-          ArrowUpIcon: true
-        });
+        setData(response);
+        setArrowUpIcon(true)
 
         // fetchs question data if dificulty and type is chosen 
 
       }  else if (dificulty && type){
         const APi_call = await axios.get(`https://opentdb.com/api.php?amount=7&difficulty=${dificulty}&type=${type}`);
         const response = APi_call.data.results;
-        this.setState({
-          data : response,
-          ArrowUpIcon: true
-        });
-      };
+        setData(response);
+        setArrowUpIcon(true)
    };
+  }
 
    // Checks if answer is Correct
-   handleAnswer(event, correctAnswer){
-      this.setState({
-        guessedAnswers : event === correctAnswer ? this.state.guessedAnswers + 1 : this.state.guessedAnswers,
-        isCorrect : event === correctAnswer ? true : false,
-        inCorrect : event !== correctAnswer ? true : false,
-      });
-    setTimeout(() => {
-        this.setState({
-          isCorrect : false,
-          inCorrect : false,
-        })
-      }, 800)  
+   const handleAnswer = (event, correctAnswer) => {
+
+    setGuessedAnswers(event === correctAnswer ? guessedAnswers + 1 : guessedAnswers);
+    setIsCorrect(event === correctAnswer ? true : false);
+    setInCorrect(event !== correctAnswer ? true : false);
+    setTimeout(() => { setIsCorrect(false); setInCorrect(false) }, 800)  
      
    };
 
    // end Test: it shows how many guessed queston have user
-   endtest(){
-     this.setState({
-       endTest : !this.state.endTest
-     })
-    }
+   const endtest = () => { setEndTest(!endTest) }
 
   // play again function
-   reset(){
-     this.setState({
-      data: [],
-      guessedAnswers : 0,
-      clickedForAnswer : true,
-      endTest : false,
-      ArrowUpIcon: false
-     });
+   const reset = () => {
+     setData([]);
+     setGuessedAnswers(0);
+     setEndTest(false);
+     setArrowUpIcon(false)
    };
-  render(){
-    const { data, isCorrect, inCorrect, endTest, guessedAnswers, ArrowUpIcon } = this.state;
+
     const questions = data && data.map((qst, indx) => (
       <Questions 
         key={indx}
         question={qst.question}
         correctAnswer={qst.correct_answer}
         incorrectAnswers={qst.incorrect_answers}
-        handleAnswer={this.handleAnswer}
+        handleAnswer={handleAnswer}
         />  
     ));
     return (
        <div className="App" id='up'>
            <h1 className='App-name'>Quiz App</h1>
-
               { /* === displays  the result on the screen ===*/ }
                 {endTest && 
                   <div className='notification'>
                     <p className='notification-msg'>{`you have ${guessedAnswers} correct answers out of ${data.length}`}</p>
-                      <button className='notification-btn' onClick={this.reset}>Play Again</button>
+                      <button className='notification-btn' onClick={reset}>Play Again</button>
                   </div>}
               {/* ====== */}
 
               <ol className={`question-container ${isCorrect ? 'winner' : ''} ${inCorrect ? 'lose' : ''}`}>
-                <Form getQuestions={this.getQuestions}/>
+                <Form getQuestions={getQuestions}/>
                     {questions}
               </ol>
 
               {/* when data is fetched arrowUpicon is visible */}
 
               {ArrowUpIcon && <a href='#top'>
-                <FontAwesomeIcon  icon={faArrowAltCircleUp} onClick={this.endtest}/>
+                <FontAwesomeIcon  icon={faArrowAltCircleUp} onClick={endtest}/>
               </a>}
        </div>
     );
-  }
 }
+
 
 export default App;
